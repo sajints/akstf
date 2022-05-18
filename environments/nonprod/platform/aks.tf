5d488e0704edace429cb0b48592b2183c0df36d7
sajints <sajints@gmail.com>
2022-05-18 19:07:30 +0200

    az aad rbac

--- environments/nonprod/platform/aks.tf
+++ environments/nonprod/platform/aks.tf
@@ -1,19 +1,8 @@
-resource "azurerm_resource_group" "example" {
-  name     = var.rgname
-  location = "eastus"
-}
 
-module "network" {
-  source              = "Azure/network/azurerm"
-  resource_group_name = azurerm_resource_group.example.name
-  address_space       = "10.0.0.0/16"
-  subnet_prefixes     = ["10.0.1.0/24"]
-  subnet_names        = ["subnet1"]
-  depends_on          = [azurerm_resource_group.example]
-}
 
 data "azuread_group" "aks_cluster_admins" {
-  display_name = "AKS-cluster-admins"
+  id = "e6d277c0-4e7f-4eab-9745-3acf027afb47"
+  # display_name = "AKS-cluster-admins"
 }
 
 module "aks" {
