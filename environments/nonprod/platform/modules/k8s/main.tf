2211d699fd98551956f60c0f488feb6c0271d6a7
sajints <sajints@gmail.com>
2022-05-22 22:29:18 +0200

    k8s deployment

--- environments/nonprod/platform/modules/k8s/main.tf
+++ environments/nonprod/platform/modules/k8s/main.tf
@@ -1,12 +1,12 @@
-# provider "kubernetes" {
-# #   config_path    = "~/.kube/config"
-# #   config_context = "my-context"
-#     load_config_file = "false"
-#     host = var.host
-#     client_certificate = var.client_certificate
-#     client_key = var.client_key
-#     cluster_ca_certificate = var.cluster_ca_certificate
-# }
+provider "kubernetes" {
+#   config_path    = "~/.kube/config"
+#   config_context = "my-context"
+    load_config_file = false
+    host = var.host
+    client_certificate = var.client_certificate
+    client_key = var.client_key
+    cluster_ca_certificate = var.cluster_ca_certificate
+}
 
 # resource "kubernetes_namespace" "example" {
 #   metadata {
