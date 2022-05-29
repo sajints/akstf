659270c1a747267a80cb7dcf4970300b5047aa2b
sajints <sajints@gmail.com>
2022-05-29 14:14:02 +0200

    providers added to k8s

--- environments/nonprod/platform/modules/k8s/version.tf
+++ environments/nonprod/platform/modules/k8s/version.tf
@@ -2,18 +2,18 @@
 # #   features {}
 # # }
 
-# # terraform {
-# #   required_providers {
-# #     azurerm = {
-# #       source  = "hashicorp/azurerm"
-# #       version = "=2.92.0"
-# #     }
-# #     kubernetes = {
-# #       source  = "hashicorp/kubernetes"
-# #       version = ">= 2.0"
-# #     }
-# #   }
-# # }
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.92.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0"
+    }
+  }
+}
 
 # terraform {
 #   required_providers {
